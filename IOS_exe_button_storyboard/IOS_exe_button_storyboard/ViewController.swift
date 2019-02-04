8c81fd6eb0c4518fd676a00c80524de345e879f0
NadinZeitoune <42942222+NadinZeitoune@users.noreply.github.com>
2019-02-04 16:09:35 +0200

    storyBoard

---
+++ IOS_exe_button_storyboard/IOS_exe_button_storyboard/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  IOS_exe_button_storyboard
+//
+//  Created by hackeru on 04/02/2019.
+//  Copyright © 2019 hackeru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var lblMassage: UILabel!
+    @IBOutlet weak var lblCounter: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func btnPlus(_ sender: Any) {
+    }
+    
+    @IBAction func btnMinus(_ sender: Any) {
+    }
+}
+
