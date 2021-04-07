ff0a1e2960b254ebd800b478d9dc20d68c704dac
NadinZeitoune <42942222+NadinZeitoune@users.noreply.github.com>
2021-04-07 15:32:36 +0300

    Create DartStructure1-5_4.dart

---
+++ DartStructure1-5_4.dart
@@ -0,0 +1,44 @@
+typedef MyCallBack = int Function(int n1, int n2);
+
+void main() {
+  int n1 = 2;
+  int n2 = 2;
+  //print(addNumbers(n1, n2));
+  //print("do $n1 + ${n2} = ${addNumbers(n2, n2)}");
+  //print("lambda => ${manipulation(n1, n2, (n1, n2) => n1 + n2)}");
+  //print("lambda2lines {} ${manipulation(n1, n2, (n1, n2) { return n1 + n2;})}");
+  MyCallBack myCallBack = (n1, n2) {
+    return n1 + n2;
+  };
+  print("typedef Function ${manipulation(n1, n2, myCallBack)}");
+}
+
+int manipulation(int n1, int n2, Function callback) {
+  return callback(n1, n2);
+}
+
+int addNumbers(int n1, int n2) {
+  return n1 + n2;
+}
+typedef MyCallBack = int Function(int n1, int n2);
+
+void main() {
+  int n1 = 2;
+  int n2 = 2;
+  //print(addNumbers(n1, n2));
+  //print("do $n1 + ${n2} = ${addNumbers(n2, n2)}");
+  //print("lambda => ${manipulation(n1, n2, (n1, n2) => n1 + n2)}");
+  //print("lambda2lines {} ${manipulation(n1, n2, (n1, n2) { return n1 + n2;})}");
+  MyCallBack myCallBack = (n1, n2) {
+    return n1 + n2;
+  };
+  print("typedef Function ${manipulation(n1, n2, myCallBack)}");
+}
+
+int manipulation(int n1, int n2, Function callback) {
+  return callback(n1, n2);
+}
+
+int addNumbers(int n1, int n2) {
+  return n1 + n2;
+}
